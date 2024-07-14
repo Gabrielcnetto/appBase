fa5698b7a7546bab608895c091fc2208ccbe19a7
Gabriel C. Netto <gabrielcarlosnetto@outlook.com>
2024-07-14 01:07:14 -0300

    ok

--- lib/managerHome/screen/manager/GraphicsAndSales/GraphicsScreenManager.dart
+++ lib/managerHome/screen/manager/GraphicsAndSales/GraphicsScreenManager.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:lionsbarberv1/classes/Estabelecimento.dart';
+import 'package:lionsbarberv1/functions/managerScreenFunctions.dart';
 import 'package:lionsbarberv1/managerHome/screen/manager/GraphicsAndSales/componentes/FaturamentoMesSelecionado.dart';
 import 'package:lionsbarberv1/managerHome/screen/manager/GraphicsAndSales/componentes/detailsList.dart';
 import 'package:lionsbarberv1/rotas/Approutes.dart';
 
 class GraphicsManagerScreen extends StatefulWidget {
   final String mesSelecionado;
-  const GraphicsManagerScreen({super.key, required this.mesSelecionado,});
+  const GraphicsManagerScreen({
+    super.key,
+    required this.mesSelecionado,
+  });
 
   @override
   State<GraphicsManagerScreen> createState() => _GraphicsManagerScreenState();
@@ -16,62 +22,100 @@ class GraphicsManagerScreen extends StatefulWidget {
 
 class _GraphicsManagerScreenState extends State<GraphicsManagerScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadTotalFaturamentoAtualMes();
+  }
+
+  bool loadingPrincipalFaturamento = false;
+  int faturamentoExibido = 0;
+  Future<void> loadTotalFaturamentoAtualMes() async {
+    setState(() {
+      loadingPrincipalFaturamento = true;
+    });
+    final DateTime dataAtual = DateTime.now();
+    await initializeDateFormatting('pt_BR');
+
+    String monthName = DateFormat('MMMM', 'pt_BR').format(dataAtual);
+    int totalFaturamentoGet = await ManagerScreenFunctions()
+        .loadFaturamentoBarbeariaSelectMenu(
+            mesSelecionado: widget.mesSelecionado == "Clique"
+                ? monthName.toLowerCase()
+                : widget.mesSelecionado.toLowerCase());
+
+    setState(() {
+      faturamentoExibido = totalFaturamentoGet;
+      loadingPrincipalFaturamento = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.of(context).pushReplacementNamed(AppRoutesApp.HomeScreen01);
-                      },
-                      child: Icon(
-                        Icons.arrow_back_ios,
-                        size: 25,
-                        color: Colors.black,
+        child: loadingPrincipalFaturamento == false
+            ? Padding(
+                padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.of(context).pushReplacementNamed(
+                                  AppRoutesApp.HomeScreen01);
+                            },
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              size: 25,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            "Estatísticas Mensais",
+                            style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.15,
+                            height: MediaQuery.of(context).size.height * 0.08,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                Estabelecimento.urlLogo,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    Text(
-                      "Estatísticas Mensais",
-                      style: GoogleFonts.openSans(
-                        textStyle: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
+                      SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.15,
-                      height: MediaQuery.of(context).size.height * 0.08,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset(
-                          Estabelecimento.urlLogo,
-                          fit: BoxFit.cover,
-                        ),
+                      FaturamentoMesSelecionado(
+                        mesInicial: widget.mesSelecionado,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 15,
+                      ),
+                      CardWithDetailsView(),
+                    ],
+                  ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                FaturamentoMesSelecionado(mesInicial: widget.mesSelecionado,),
-                SizedBox(height: 15,),
-                CardWithDetailsView(),
-              ],
-            ),
-          ),
-        ),
+              )
+            : Center(
+                child: CircularProgressIndicator.adaptive(),
+              ),
       ),
     );
   }
--- lib/managerHome/screen/manager/GraphicsAndSales/screens/media_horarios_preenchidos.dart
+++ lib/managerHome/screen/manager/GraphicsAndSales/screens/media_horarios_preenchidos.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lionsbarberv1/classes/Estabelecimento.dart';
+import 'package:lionsbarberv1/rotas/Approutes.dart';
 
 class MediaHorariosPreenchidos extends StatelessWidget {
   const MediaHorariosPreenchidos({super.key});
@@ -8,9 +11,47 @@ class MediaHorariosPreenchidos extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: Text(
-            "Horários preenchidos",
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15,right: 15,top: 15),
+          child: Column(
+            children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 25,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "Buracos na Agenda",
+                      style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.15,
+                      height: MediaQuery.of(context).size.height * 0.08,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          Estabelecimento.urlLogo,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+            ],
           ),
         ),
       ),
